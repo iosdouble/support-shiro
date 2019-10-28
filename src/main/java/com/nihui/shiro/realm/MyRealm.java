d5752fdaa7620f3d31b1322179207262281e5b19
nihui <18202504057@163.com>
2019-10-28 14:50:33 +0800

    Shiro身份认证以及Realm

---
+++ src/main/java/com/nihui/shiro/realm/MyRealm.java
@@ -0,0 +1,36 @@
+package com.nihui.shiro.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @Classname MyRealm
+ * @Description TODO
+ * @Date 2019/10/28 2:02 PM
+ * @Created by nihui
+ */
+public class MyRealm implements Realm {
+
+    public String getName() {
+        return "myrealm";
+    }
+    public boolean supports(AuthenticationToken token) {
+        //仅仅支持一个UsernamePaasswordToke
+        return token instanceof UsernamePasswordToken;
+    }
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        //获取到用户名和密码
+        String username = (String) token.getPrincipal();
+        String password = new String((char[]) token.getCredentials());
+        if (!"nihui".equals(username)){
+            //用户名错误
+            throw new UnknownAccountException();
+        }
+        if (!"123".equals(password)){
+            //密码错误
+            throw new IncorrectCredentialsException();
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo
+        return new SimpleAuthenticationInfo(username,password,getName());
+    }
+}
---
+++ src/main/java/com/nihui/shiro/realm/SRealeTest.java
@@ -0,0 +1,42 @@
+package com.nihui.shiro.realm;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
+
+/**
+ * @Classname LoginLogoutTest
+ * @Description TODO
+ * @Date 2019/10/28 10:59 AM
+ * @Created by nihui
+ */
+public class SRealeTest {
+    public static void main(String[] args) {
+        //1、获取SecurityManager工厂，
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-realm.ini");
+        //2、得到一个SecurityManager实例，绑定到SecurityUtils
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //得到Subject 以及用户名密码的身份验证Token
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("nihui","123");
+        // 验证登陆
+
+        try {
+            subject.login(token);
+        }catch (AuthenticationException e){
+            //身份认证失败
+        }
+        System.out.println(subject.isAuthenticated()); //true表示用户已经登陆
+
+        //退出操作
+        subject.logout();
+
+    }
+}
